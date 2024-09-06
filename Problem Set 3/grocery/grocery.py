e67823e37762bfade831b08df389f74615b32ce0
ismailadadas <ismailadadas@gmail.com>
2024-09-07 02:19:50 +0700

    editing grocery.py

--- Problem Set 3/grocery/grocery.py
+++ Problem Set 3/grocery/grocery.py
@@ -1,9 +1,19 @@
-'''
-try:
-    item = input()
-except EOFError:
-    ...
-    if key in d:
-    ...
-;;
-'''
+grocery = {}
+
+while True:
+    try: 
+
+        item = input()
+
+        if item in grocery:
+            grocery[item] += 1 
+
+        else : 
+            grocery[item] = 1 
+    except EOFError:
+
+        for key in sorted(grocery.keys()):
+            print(grocery[key], key.upper())
+           
+
+        break
