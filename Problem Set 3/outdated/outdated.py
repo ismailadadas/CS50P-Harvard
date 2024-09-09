8f64b3d5490d26b3c5b0214f63adc4c5edc2f391
ismailadadas <ismailadadas@gmail.com>
2024-09-09 23:59:52 +0700

    editing outdated.py

--- Problem Set 3/outdated/outdated.py
+++ Problem Set 3/outdated/outdated.py
@@ -18,3 +18,28 @@ while True:
 
     if "/" in date:
         month, day, year = date.split("/")
+
+    elif "," in date:
+        month, day, year = date.split()
+
+        if month in months: 
+            month = months.index(month)
+            month += 1 
+            day = day.strip(",")
+
+        else:
+            continue
+        try:
+
+            if int(day) > 31 or int(month) > 12: 
+                continue
+
+            else: 
+                break
+
+        except ValueError:
+            continue
+month = int(month)
+day = int(day)
+
+print(f"{year}-{month:02}-{day:02}")
