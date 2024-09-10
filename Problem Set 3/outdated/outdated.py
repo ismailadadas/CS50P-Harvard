b481323be433ba3ba85131bdacaaea3a5e5411b4
ismailadadas <ismailadadas@gmail.com>
2024-09-10 23:50:07 +0700

    editing outdated.py

--- Problem Set 3/outdated/outdated.py
+++ Problem Set 3/outdated/outdated.py
@@ -13,33 +13,37 @@ months = [
     "December"
 ]
 
-while True: 
+while True:
     date = input("Date: ").strip()
 
     if "/" in date:
-        month, day, year = date.split("/")
+        try:
+            month, day, year = date.split("/")
+            month = int(month)
+            day = int(day)
+            year = int(year)
+        except ValueError:
+            continue  
 
     elif "," in date:
-        month, day, year = date.split()
-
-        if month in months: 
-            month = months.index(month)
-            month += 1 
-            day = day.strip(",")
-
-        else:
-            continue
         try:
-
-            if int(day) > 31 or int(month) > 12: 
+            month_name, day, year = date.split()
+            if month_name in months:
+                month = months.index(month_name) + 1
+                day = int(day.strip(","))
+                year = int(year)
+            else:
                 continue
-
-            else: 
-                break
-
         except ValueError:
             continue
-month = int(month)
-day = int(day)
+
+    else:
+        continue
+
+
+    if not (1 <= month <= 12 and 1 <= day <= 31):
+        continue
+
+    break
 
 print(f"{year}-{month:02}-{day:02}")
