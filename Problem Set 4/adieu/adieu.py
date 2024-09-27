ec8507814eb73bc945fa57ad62af88ef5e2e54ca
ismailadadas <ismailadadas@gmail.com>
2024-09-28 01:59:59 +0700

    editing adieu.py

--- Problem Set 4/adieu/adieu.py
+++ Problem Set 4/adieu/adieu.py
@@ -1,15 +1,20 @@
-import inflect 
+import inflect
 
 p = inflect.engine()
 
 names = []
 
-while True : 
-    try: 
-        name = input("Name:")
-        names.apppend(name)
-    except EOFError:
+# Input loop to collect names
+while True:
+    try:
+        name = input("Name: ")
+        names.append(name)  # Corrected typo from apppend to append
+    except EOFError:  # Control-D (EOF) to stop input
         print()
         break
+
+# Using inflect engine to join names
 output = p.join(names)
-print("Audieu , audieu, to " + output) 
+
+# Output the result
+print("Adieu, adieu, to " + output)
