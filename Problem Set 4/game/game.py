283eaf46aba62fbceda74b71098263e12ef984ff
ismailadadas <ismailadadas@gmail.com>
2024-09-30 23:20:43 +0700

    editing game.py

--- Problem Set 4/game/game.py
+++ Problem Set 4/game/game.py
@@ -1,1 +1,21 @@
-print = game
+import random 
+
+keepLooping = True
+while keepLooping :
+    try:
+        n = int(input("Level: "))
+        num = random.randint(1, n)
+        guess = int(input("Guess "))
+
+        if num < guess:
+            print("too large!")
+
+        elif num > guess:
+            print("too small!")
+
+        elif num == guess: 
+            print("just right!")
+            keepLooping = False
+
+    except ValueError:
+        continue 
