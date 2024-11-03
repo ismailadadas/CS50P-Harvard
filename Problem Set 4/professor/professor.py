0d1c67ae16eeaf5b322ce96cdd6c2f4b6beab7b8
ismailadadas <ismailadadas@gmail.com>
2024-11-03 23:02:48 +0700

    editing pproffesor.py

--- Problem Set 4/professor/professor.py
+++ Problem Set 4/professor/professor.py
@@ -2,7 +2,10 @@ import random
 
 
 def main():
-    ...
+    score = 0 
+    no_of_tries = 0
+    i = 0
+    level = get_level()
 
 
 def get_level():
@@ -11,13 +14,23 @@ def get_level():
         try:
             level = int(input("Level: "))
             if level in [1,2,3,4]:
-                break
-        except
+                return level
+            else:
+                continue
+        except ValueError:
+            continue
 
 
 def generate_integer(level):
-    ...
-
+    if level == 1: 
+        x = random.randint(0,9)
+        y = random.randint(0,9)
+    elif level == 2:
+        x = random.randint(10,99)
+        y = random.randint(10,99)
+    else:
+        x = random.random.randint(100,999)
+        y = random.random.randint(100,999)
 
 if __name__ == "__main__":
     main() 
